26320fe1988b9e88d734e28d55d70274b2c3e61d
Martin Erck <unknownmle107@gmail.com>
2019-10-24 16:55:58 -0500

    Finished the card class, and added the basic elements of the deck class

--- DavesBlackjack/DavesBlackjack/Card.cs
+++ DavesBlackjack/DavesBlackjack/Card.cs
@@ -6,37 +6,42 @@ using System.Threading.Tasks;
 
 namespace DavesBlackjack
 {
-    class Card
+    /// <summary>
+    /// Contains methods and fields belonging to an individual card
+    /// </summary>
+    public class Card
     {
-        private int id;
-        private string suit;
+        private readonly int id; //Used mainly for card creation
+        private int value; //The value the card has during gameplay
+        private string suit; //The suit of the card
+        private string imageFilePath; // The filepath for the card's image
 
         public Card(int id)
         {
             this.id = id;
+            this.ParseCardId();
         }
 
-
-        private void ParseCardValue()
+        /// <summary>
+        /// Takes the id of the card that called this function, and assigns card values based on the id
+        /// </summary>
+        private void ParseCardId()
         {
             int suit = id / 13;
             if(suit == 0)
-            {
                 this.suit = "Clubs";
-            }
             else if( suit == 1)
-            {
                 this.suit = "Spades";
-            }
             else if(suit == 2)
-            {
                 this.suit = "Hearts";
-            }
             else
-            {
                 this.suit = "Diamonds";
-            }
 
-
+            int parsevalue = ( this.id + 1 ) % 13;
+            if (parsevalue > 0 && parsevalue <= 10)
+                this.value = parsevalue;
+            else
+                this.value = 10;
+        }
     }
 }
---
+++ DavesBlackjack/DavesBlackjack/Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavesBlackjack
+{
+    /// <summary>
+    /// Holds the 52 cards used during gameplay
+    /// </summary>
+    class Deck
+    {
+        /// <summary>
+        /// List of cards using the Card class
+        /// </summary>
+        private List<Card> Cards;
+        public Deck()
+        {
+            this.InitializeDeck();
+        }
+        /// <summary>
+        /// Initializes the deck by adding one of each card to the deck, and then shuffling the deck
+        /// </summary>
+        public void InitializeDeck()
+        {
+            for (int x = 0; x <= 51; x++ )
+            {
+                this.Cards.Clear();
+                this.Cards.Add(new Card(x));
+            }
+
+            //Shuffling the deck goes here
+        }
+    }
+}
