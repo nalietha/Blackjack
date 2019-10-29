336871df5c2558a23b63c343a1505207b3739fa4
J-WEEB <jordan.haas0@gmail.com>
2019-10-29 14:18:15 -0500

    form1

--- DavesBlackjack/DavesBlackjack/Program.cs
+++ DavesBlackjack/DavesBlackjack/Program.cs
@@ -16,7 +16,7 @@ namespace DavesBlackjack
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BlackJack());
+            Application.Run(new Form1());
         }
     }
 }
