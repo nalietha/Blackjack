12e80e56bd33950dbf5f6411fa5343fbcc70e187
Nathan Lietha <Nathan.Lietha@outlook.com>
2019-10-31 11:11:46 -0500

    Added funtionality to Player and Dealer classes

--- DavesBlackjack/DavesBlackjack/Dealer.cs
+++ DavesBlackjack/DavesBlackjack/Dealer.cs
@@ -8,20 +8,31 @@ namespace DavesBlackjack
 {
     class Dealer : Player
     {
-        private int[] _dealerhand = { 0 };
+        // private int[] _dealerHand = { 0 };
+        private List<Card> _dealersCardList = new List<Card>();
         private int currentHand = 0;
+
+        private int _dealerHandValue = 0;
+
+        public Dealer(List<DealerDeck> currCard)
+        {
+            _dealersCardList.Add(currCard);
+        }
 
         private void Choice()
         {
+            currentHand = CalcuateCurrentHand(_dealersCardList);
             // check hand
-            if (currentHand > 16)
+            if (currentHand < 17)
                 Hit();
             else
                 Stay();
         }
 
-
-
+        public override void Hit()
+        {
+            _dealersCardList.Add(Deck.GetNextCard());
+        }
 
     }
 }
