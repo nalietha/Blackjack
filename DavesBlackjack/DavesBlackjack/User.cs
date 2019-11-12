e1c2b7e346c0a186ee661eaf5e8b4f43f86fc8d4
Nathan Lietha <Nathan.Lietha@outlook.com>
2019-11-12 13:16:25 -0600

    Added more functions

--- DavesBlackjack/DavesBlackjack/User.cs
+++ DavesBlackjack/DavesBlackjack/User.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DavesBlackjack
 {
     class User
     {
-        public User()
+        public User(string username, string hPass)
         {
-            
+            _username = username;
+            _hPass = hPass;
         }
 
         public string _username;
+        private string _hPass;
+        public string database = "Database.xml";
+
+
 
         public string GetUsername()
         {
@@ -21,12 +27,48 @@ namespace DavesBlackjack
             return this._username;
         }
 
+
         public decimal GetUserMoney()
         {
             // Parse XML file with username to find amount of cash the the user has left
 
 
             return (decimal)1000.00;
+        }
+        public int GetUserGameWins()
+        {
+            // 
+
+
+
+            return 0;
+        }
+        public bool GetSavedGame()
+        {
+            // Check if there is a saved game
+            if (true)
+                return false;
+            // Saved deck
+
+            // Get users saved game card sets
+            // Hand
+            GetPlayersHand();
+
+            // Get dealers saved game card sets
+            // Hand
+            GetDealersHand();
+            return true;
+        }
+        public List<Card> GetPlayersHand()
+        {
+
+            return null;
+        }
+
+        public List<Card> GetDealersHand()
+        {
+            return null;
+
         }
 
         public bool ChangeUserPassword()
