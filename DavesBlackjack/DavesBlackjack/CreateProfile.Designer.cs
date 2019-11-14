ebd67d3aa54f573c8a00133b494df942c3bb368d
Nathan Lietha <Nathan.Lietha@outlook.com>
2019-11-14 14:05:22 -0600

    CreateNewUser added code (not finished)

--- DavesBlackjack/DavesBlackjack/CreateProfile.Designer.cs
+++ DavesBlackjack/DavesBlackjack/CreateProfile.Designer.cs
@@ -30,36 +30,110 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.backButton = new System.Windows.Forms.Button();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(233, 38);
+            this.label1.Location = new System.Drawing.Point(414, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(278, 46);
+            this.label1.Size = new System.Drawing.Size(453, 76);
             this.label1.TabIndex = 0;
             this.label1.Text = "Create Profile";
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(290, 282);
+            this.backButton.Location = new System.Drawing.Point(794, 590);
+            this.backButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(104, 43);
+            this.backButton.Size = new System.Drawing.Size(185, 67);
             this.backButton.TabIndex = 1;
             this.backButton.Text = "back";
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(542, 586);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(200, 71);
+            this.btnCreateAccount.TabIndex = 2;
+            this.btnCreateAccount.Text = "Create";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(437, 194);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(430, 38);
+            this.tbUsername.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(437, 296);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(430, 38);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(431, 159);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(153, 32);
+            this.lblUsername.TabIndex = 5;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(437, 258);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(139, 32);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(437, 354);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(245, 32);
+            this.lblConfirmPassword.TabIndex = 7;
+            this.lblConfirmPassword.Text = "Confirm Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(437, 403);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 38);
+            this.textBox1.TabIndex = 8;
+            // 
             // CreateProfile
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1422, 697);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.btnCreateAccount);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "CreateProfile";
             this.Text = "CreateProfile";
             this.ResumeLayout(false);
@@ -71,5 +145,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- DavesBlackjack/DavesBlackjack/CreateProfile.cs
+++ DavesBlackjack/DavesBlackjack/CreateProfile.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace DavesBlackjack
 {
@@ -16,10 +17,91 @@ namespace DavesBlackjack
         {
             InitializeComponent();
         }
+        private string DatabaseFile = "Database.xml";
 
         private void backButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        // Error Messages
+        public string EMPTY_USER_NAME = "Please enter a username";
+        public string USER_ALREADY_EXISTS = "ERROR: User already exists, try another username";
+        public string PASSWORD_TOO_SHORT = "Password is too short, try a longer password";
+        public string PASSWORD_MISMATCH = "Passwords do not match";
+        public string PASSWORD_EMPTY = "Please enter password";
+
+
+        public string CreateNewUser()
+        {
+            string saltVal = "Hello";
+            XDocument doc = XDocument.Load(DatabaseFile);
+
+            // Check if boxes are filled.
+            // Username
+            if (tbUsername.Text == "") 
+                return EMPTY_USER_NAME;
+
+            // Check if username is already used (case insesitive)
+           /* if (tbUsername.Text.ToLower() == *//*search*//*)
+                return USER_ALREADY_EXISTS;*/
+
+
+            // Password
+            // Not Empty
+            if (tbPassword.Text == "")
+            {
+                return PASSWORD_MISMATCH;
+            }
+            // Minimum Length
+
+            // Matches Second
+
+            // Security Question
+
+            // Check Password longer then 8 charecters and no spaces
+
+            // Hash Password
+
+            // Add User to XML Database
+            // Open document
+
+            // Build XML tree
+            // Generate Salt for user
+
+            int currentUsers = doc.Descendants("Users").Count();
+            XElement newUser = new XElement("Username",
+                new XAttribute("id", currentUsers + 1),
+                new XAttribute("uName", tbUsername),
+                new XAttribute("Salt", saltVal),
+                new XAttribute("password", tbPassword.GetHashCode()),
+                new XElement("SecurityQuestions",
+                    new XElement("Question"),
+                    new XElement("Answer")),
+                new XElement("PaymentInfo",
+                    new XElement("CardNumber"),
+                    new XElement("NameOnCard"),
+                    new XElement("SecurityCode"),
+                    new XElement("ExpireDate")),
+                new XElement("GamesWon"),
+                new XElement("Cash"),
+                new XElement("GameInProgress",
+                    new XElement("Status"),
+                    new XElement("SaveState"))
+                );
+
+            // Load into Root Users
+            doc.Root.Element("Users").Add(newUser);
+
+
+
+
+            // If any errors are present, return "Failure" with msg.
+
+
+            return "Success";
+        }
+
+
     }
 }
