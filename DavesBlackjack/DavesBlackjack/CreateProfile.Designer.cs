c2e0241971ba66f4ac53268494f52301d7962a82
Nathan Lietha <Nathan.Lietha@outlook.com>
2019-11-19 14:08:03 -0600

    Create Profile update, Add user completed, Error checking uncompleted

--- DavesBlackjack/DavesBlackjack/CreateProfile.Designer.cs
+++ DavesBlackjack/DavesBlackjack/CreateProfile.Designer.cs
@@ -30,64 +30,72 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateProfile));
             this.label1 = new System.Windows.Forms.Label();
-            this.backButton = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.btnCreateAccount = new System.Windows.Forms.Button();
             this.tbUsername = new System.Windows.Forms.TextBox();
             this.tbPassword = new System.Windows.Forms.TextBox();
             this.lblUsername = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.lblConfirmPassword = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbPasswordConfirm = new System.Windows.Forms.TextBox();
             this.cbSecurityQuestion = new System.Windows.Forms.ComboBox();
             this.lblSecuirtyQuestionChoice = new System.Windows.Forms.Label();
             this.lblSecurityQuestionAnswer = new System.Windows.Forms.Label();
             this.tbSecuirtyQuestionAnswer = new System.Windows.Forms.TextBox();
+            this.lblUsernameError = new System.Windows.Forms.Label();
+            this.lblPasswordError = new System.Windows.Forms.Label();
+            this.lblConfirmError = new System.Windows.Forms.Label();
+            this.lblSecuirtyQuestionError = new System.Windows.Forms.Label();
+            this.lblSelectSecuirtyError = new System.Windows.Forms.Label();
+            this.pnlUsername = new System.Windows.Forms.Panel();
+            this.btnDebugPopulate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(414, 58);
+            this.label1.Location = new System.Drawing.Point(26, 67);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(453, 76);
             this.label1.TabIndex = 0;
             this.label1.Text = "Create Profile";
             // 
-            // backButton
+            // btnCancel
             // 
-            this.backButton.Location = new System.Drawing.Point(516, 438);
-            this.backButton.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
-            this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(184, 66);
-            this.backButton.TabIndex = 1;
-            this.backButton.Text = "back";
-            this.backButton.UseVisualStyleBackColor = true;
-            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            this.btnCancel.Location = new System.Drawing.Point(398, 746);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(184, 66);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.backButton_Click);
             // 
             // btnCreateAccount
             // 
-            this.btnCreateAccount.Location = new System.Drawing.Point(1084, 1135);
-            this.btnCreateAccount.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnCreateAccount.Location = new System.Drawing.Point(594, 746);
+            this.btnCreateAccount.Margin = new System.Windows.Forms.Padding(6);
             this.btnCreateAccount.Name = "btnCreateAccount";
-            this.btnCreateAccount.Size = new System.Drawing.Size(400, 138);
+            this.btnCreateAccount.Size = new System.Drawing.Size(295, 64);
             this.btnCreateAccount.TabIndex = 2;
             this.btnCreateAccount.Text = "Create";
             this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
             // 
             // tbUsername
             // 
-            this.tbUsername.Location = new System.Drawing.Point(874, 376);
-            this.tbUsername.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tbUsername.Location = new System.Drawing.Point(33, 221);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(6);
             this.tbUsername.Name = "tbUsername";
             this.tbUsername.Size = new System.Drawing.Size(856, 38);
             this.tbUsername.TabIndex = 3;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(874, 574);
-            this.tbPassword.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tbPassword.Location = new System.Drawing.Point(33, 329);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(6);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(856, 38);
             this.tbPassword.TabIndex = 4;
@@ -95,7 +103,7 @@
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(862, 308);
+            this.lblUsername.Location = new System.Drawing.Point(33, 177);
             this.lblUsername.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(153, 32);
@@ -105,30 +113,30 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(874, 500);
+            this.lblPassword.Location = new System.Drawing.Point(33, 291);
             this.lblPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(139, 32);
+            this.lblPassword.Size = new System.Drawing.Size(147, 32);
             this.lblPassword.TabIndex = 6;
-            this.lblPassword.Text = "Password";
+            this.lblPassword.Text = "Password:";
             // 
             // lblConfirmPassword
             // 
             this.lblConfirmPassword.AutoSize = true;
-            this.lblConfirmPassword.Location = new System.Drawing.Point(874, 686);
+            this.lblConfirmPassword.Location = new System.Drawing.Point(33, 406);
             this.lblConfirmPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblConfirmPassword.Name = "lblConfirmPassword";
             this.lblConfirmPassword.Size = new System.Drawing.Size(245, 32);
             this.lblConfirmPassword.TabIndex = 7;
             this.lblConfirmPassword.Text = "Confirm Password";
             // 
-            // textBox1
+            // tbPasswordConfirm
             // 
-            this.textBox1.Location = new System.Drawing.Point(874, 781);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(856, 38);
-            this.textBox1.TabIndex = 8;
+            this.tbPasswordConfirm.Location = new System.Drawing.Point(33, 444);
+            this.tbPasswordConfirm.Margin = new System.Windows.Forms.Padding(6);
+            this.tbPasswordConfirm.Name = "tbPasswordConfirm";
+            this.tbPasswordConfirm.Size = new System.Drawing.Size(856, 38);
+            this.tbPasswordConfirm.TabIndex = 8;
             // 
             // cbSecurityQuestion
             // 
@@ -144,7 +152,7 @@
             "What is your grandmother\'s (on your mother\'s side) maiden name?",
             "What is your spouse or partner\'s mother\'s maiden name?",
             "In what town or city did your mother and father meet?"});
-            this.cbSecurityQuestion.Location = new System.Drawing.Point(868, 898);
+            this.cbSecurityQuestion.Location = new System.Drawing.Point(33, 553);
             this.cbSecurityQuestion.Name = "cbSecurityQuestion";
             this.cbSecurityQuestion.Size = new System.Drawing.Size(856, 39);
             this.cbSecurityQuestion.TabIndex = 9;
@@ -152,7 +160,7 @@
             // lblSecuirtyQuestionChoice
             // 
             this.lblSecuirtyQuestionChoice.AutoSize = true;
-            this.lblSecuirtyQuestionChoice.Location = new System.Drawing.Point(868, 860);
+            this.lblSecuirtyQuestionChoice.Location = new System.Drawing.Point(33, 515);
             this.lblSecuirtyQuestionChoice.Name = "lblSecuirtyQuestionChoice";
             this.lblSecuirtyQuestionChoice.Size = new System.Drawing.Size(327, 32);
             this.lblSecuirtyQuestionChoice.TabIndex = 10;
@@ -161,42 +169,119 @@
             // lblSecurityQuestionAnswer
             // 
             this.lblSecurityQuestionAnswer.AutoSize = true;
-            this.lblSecurityQuestionAnswer.Location = new System.Drawing.Point(868, 970);
+            this.lblSecurityQuestionAnswer.Location = new System.Drawing.Point(33, 625);
             this.lblSecurityQuestionAnswer.Name = "lblSecurityQuestionAnswer";
-            this.lblSecurityQuestionAnswer.Size = new System.Drawing.Size(109, 32);
+            this.lblSecurityQuestionAnswer.Size = new System.Drawing.Size(117, 32);
             this.lblSecurityQuestionAnswer.TabIndex = 11;
-            this.lblSecurityQuestionAnswer.Text = "Answer";
+            this.lblSecurityQuestionAnswer.Text = "Answer:";
             // 
             // tbSecuirtyQuestionAnswer
             // 
-            this.tbSecuirtyQuestionAnswer.Location = new System.Drawing.Point(874, 1015);
+            this.tbSecuirtyQuestionAnswer.Location = new System.Drawing.Point(39, 670);
             this.tbSecuirtyQuestionAnswer.Name = "tbSecuirtyQuestionAnswer";
             this.tbSecuirtyQuestionAnswer.Size = new System.Drawing.Size(850, 38);
             this.tbSecuirtyQuestionAnswer.TabIndex = 12;
+            // 
+            // lblUsernameError
+            // 
+            this.lblUsernameError.AutoSize = true;
+            this.lblUsernameError.Location = new System.Drawing.Point(899, 221);
+            this.lblUsernameError.Name = "lblUsernameError";
+            this.lblUsernameError.Size = new System.Drawing.Size(34, 32);
+            this.lblUsernameError.TabIndex = 13;
+            this.lblUsernameError.Text = "X";
+            this.lblUsernameError.Visible = false;
+            // 
+            // lblPasswordError
+            // 
+            this.lblPasswordError.AutoSize = true;
+            this.lblPasswordError.Location = new System.Drawing.Point(905, 334);
+            this.lblPasswordError.Name = "lblPasswordError";
+            this.lblPasswordError.Size = new System.Drawing.Size(34, 32);
+            this.lblPasswordError.TabIndex = 14;
+            this.lblPasswordError.Text = "X";
+            this.lblPasswordError.Visible = false;
+            // 
+            // lblConfirmError
+            // 
+            this.lblConfirmError.AutoSize = true;
+            this.lblConfirmError.Location = new System.Drawing.Point(905, 444);
+            this.lblConfirmError.Name = "lblConfirmError";
+            this.lblConfirmError.Size = new System.Drawing.Size(34, 32);
+            this.lblConfirmError.TabIndex = 15;
+            this.lblConfirmError.Text = "X";
+            this.lblConfirmError.Visible = false;
+            // 
+            // lblSecuirtyQuestionError
+            // 
+            this.lblSecuirtyQuestionError.AutoSize = true;
+            this.lblSecuirtyQuestionError.Location = new System.Drawing.Point(905, 670);
+            this.lblSecuirtyQuestionError.Name = "lblSecuirtyQuestionError";
+            this.lblSecuirtyQuestionError.Size = new System.Drawing.Size(34, 32);
+            this.lblSecuirtyQuestionError.TabIndex = 16;
+            this.lblSecuirtyQuestionError.Text = "X";
+            this.lblSecuirtyQuestionError.Visible = false;
+            // 
+            // lblSelectSecuirtyError
+            // 
+            this.lblSelectSecuirtyError.AutoSize = true;
+            this.lblSelectSecuirtyError.Location = new System.Drawing.Point(899, 556);
+            this.lblSelectSecuirtyError.Name = "lblSelectSecuirtyError";
+            this.lblSelectSecuirtyError.Size = new System.Drawing.Size(34, 32);
+            this.lblSelectSecuirtyError.TabIndex = 17;
+            this.lblSelectSecuirtyError.Text = "X";
+            this.lblSelectSecuirtyError.Visible = false;
+            // 
+            // pnlUsername
+            // 
+            this.pnlUsername.Location = new System.Drawing.Point(33, 212);
+            this.pnlUsername.Name = "pnlUsername";
+            this.pnlUsername.Size = new System.Drawing.Size(860, 55);
+            this.pnlUsername.TabIndex = 18;
+            // 
+            // btnDebugPopulate
+            // 
+            this.btnDebugPopulate.Location = new System.Drawing.Point(594, 820);
+            this.btnDebugPopulate.Name = "btnDebugPopulate";
+            this.btnDebugPopulate.Size = new System.Drawing.Size(295, 54);
+            this.btnDebugPopulate.TabIndex = 19;
+            this.btnDebugPopulate.Text = "Populate";
+            this.btnDebugPopulate.UseVisualStyleBackColor = true;
+            this.btnDebugPopulate.Click += new System.EventHandler(this.btnDebugPopulate_Click);
             // 
             // CreateProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
-            this.ClientSize = new System.Drawing.Size(2844, 1350);
+            this.ClientSize = new System.Drawing.Size(1471, 1036);
+            this.Controls.Add(this.btnDebugPopulate);
+            this.Controls.Add(this.lblSelectSecuirtyError);
+            this.Controls.Add(this.lblSecuirtyQuestionError);
+            this.Controls.Add(this.lblConfirmError);
+            this.Controls.Add(this.lblPasswordError);
+            this.Controls.Add(this.lblUsernameError);
             this.Controls.Add(this.tbSecuirtyQuestionAnswer);
             this.Controls.Add(this.lblSecurityQuestionAnswer);
             this.Controls.Add(this.lblSecuirtyQuestionChoice);
             this.Controls.Add(this.cbSecurityQuestion);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbPasswordConfirm);
             this.Controls.Add(this.lblConfirmPassword);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUsername);
             this.Controls.Add(this.btnCreateAccount);
-            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlUsername);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "CreateProfile";
             this.Text = "CreateProfile";
+            this.Load += new System.EventHandler(this.CreateProfile_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,17 +290,24 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnCreateAccount;
         private System.Windows.Forms.TextBox tbUsername;
         private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label lblConfirmPassword;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbPasswordConfirm;
         private System.Windows.Forms.ComboBox cbSecurityQuestion;
         private System.Windows.Forms.Label lblSecuirtyQuestionChoice;
         private System.Windows.Forms.Label lblSecurityQuestionAnswer;
         private System.Windows.Forms.TextBox tbSecuirtyQuestionAnswer;
+        private System.Windows.Forms.Label lblUsernameError;
+        private System.Windows.Forms.Label lblPasswordError;
+        private System.Windows.Forms.Label lblConfirmError;
+        private System.Windows.Forms.Label lblSecuirtyQuestionError;
+        private System.Windows.Forms.Label lblSelectSecuirtyError;
+        private System.Windows.Forms.Panel pnlUsername;
+        private System.Windows.Forms.Button btnDebugPopulate;
     }
 }
--- DavesBlackjack/DavesBlackjack/CreateProfile.cs
+++ DavesBlackjack/DavesBlackjack/CreateProfile.cs
@@ -17,12 +17,10 @@ namespace DavesBlackjack
         {
             InitializeComponent();
         }
-        private string DatabaseFile = "Database.xml";
+        
 
-        private void backButton_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+        private static string DatabaseFile = "..\\..\\Database.xml";
+        XDocument doc = XDocument.Load(DatabaseFile);
 
         // Error Messages
         public string EMPTY_USER_NAME = "Please enter a username";
@@ -30,41 +28,31 @@ namespace DavesBlackjack
         public string PASSWORD_TOO_SHORT = "Password is too short, try a longer password";
         public string PASSWORD_MISMATCH = "Passwords do not match";
         public string PASSWORD_EMPTY = "Please enter password";
+        public string PASSWORD_INVALID_CHARECTERS = "Password cannot contain spaces.";
+        public string SECURITY_QUESTION_EMPTY = "Secuirty question cannot be blank.";
+        public string USERNAME_INVAILD_CHARECTER = "Username cannot contain spaces.";
+
+        public void DebugUser()
+        {
+            tbUsername.Text = "NaLietha";
+            tbPassword.Text = "Password1!";
+            tbPasswordConfirm.Text = "Password1!";
+            tbSecuirtyQuestionAnswer.Text = "Answer";
+        }
+
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
 
         public string CreateNewUser()
         {
             string saltVal = "Hello";
-            XDocument doc = XDocument.Load(DatabaseFile);
-
-            // Check if boxes are filled.
-            // Username
-            if (tbUsername.Text == "") 
-                return EMPTY_USER_NAME;
-
-            // Check if username is already used (case insesitive)
-           /* if (tbUsername.Text.ToLower() == *//*search*//*)
-                return USER_ALREADY_EXISTS;*/
-
-
-            // Password
-            // Not Empty
-            if (tbPassword.Text == "")
-            {
-                return PASSWORD_MISMATCH;
-            }
-            // Minimum Length
-
-            // Matches Second
-
-            // Security Question
-
-            // Check Password longer then 8 charecters and no spaces
 
             // Hash Password
-
-            // Add User to XML Database
-            // Open document
+            var hashPass = tbPassword.Text;
 
             // Build XML tree
             // Generate Salt for user
@@ -72,36 +60,144 @@ namespace DavesBlackjack
             int currentUsers = doc.Descendants("Users").Count();
             XElement newUser = new XElement("Username",
                 new XAttribute("id", currentUsers + 1),
-                new XAttribute("uName", tbUsername),
+                new XAttribute("uName", tbUsername.Text.ToLower()),
                 new XAttribute("Salt", saltVal),
-                new XAttribute("password", tbPassword.GetHashCode()),
+                new XAttribute("password", tbPassword.Text),
+                new XElement("PlayerName", tbUsername.Text),
                 new XElement("SecurityQuestions",
-                    new XElement("Question"),
-                    new XElement("Answer")),
+                    new XElement("Question", cbSecurityQuestion.SelectedValue.ToString() ),
+                    new XElement("Answer", tbSecuirtyQuestionAnswer.Text)),
                 new XElement("PaymentInfo",
-                    new XElement("CardNumber"),
-                    new XElement("NameOnCard"),
-                    new XElement("SecurityCode"),
-                    new XElement("ExpireDate")),
-                new XElement("GamesWon"),
-                new XElement("Cash"),
+                    new XElement("CardNumber", null),
+                    new XElement("NameOnCard", null),
+                    new XElement("SecurityCode",null),
+                    new XElement("ExpireDate",null)),
+                new XElement("GamesWon", 0),
+                new XElement("Cash", 0),
                 new XElement("GameInProgress",
-                    new XElement("Status"),
-                    new XElement("SaveState"))
-                );
+                    new XElement("Status", false),
+                    new XElement("PlayerSave",
+                        new XElement("SaveState"),
+                    new XElement("DealerSave"),
+                        new XElement("SaveState"))
+                ));
 
             // Load into Root Users
-            doc.Root.Element("Users").Add(newUser);
-
-
-
+            doc.Root.Add(newUser);
+            doc.Save(DatabaseFile);
 
             // If any errors are present, return "Failure" with msg.
-
-
+            MessageBox.Show("User Created!", "Successfully created user " + tbUsername.Text);
+            this.Close();
             return "Success";
         }
+        private void ResetErrors()
+        {
+            // labels
 
 
+
+            // Panels
+        }
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            CreateNewUser();
+
+        }
+        private void CheckForErrors()
+        {
+            bool errorsExist = false;
+            // Reset Errors
+            ResetErrors();
+            // Username
+            if (tbUsername.Text == "")
+            {
+                lblUsernameError.Text = EMPTY_USER_NAME;
+                lblUsernameError.Visible = true;
+                pnlUsername.BackColor = Color.Red;
+                errorsExist = true;
+            }
+            else if (tbUsername.Text.Contains(" "))
+            {
+                lblUsernameError.Text = USERNAME_INVAILD_CHARECTER;
+                lblUsernameError.Visible = true;
+            }
+            // Check if username is already used (case insesitive)
+            else if (FindUser(tbUsername.Text))
+            {
+                lblUsernameError.Text = USER_ALREADY_EXISTS;
+                lblUsernameError.Visible = true;
+                errorsExist = true;
+            }
+            else if (tbPassword.Text == "")
+            {
+                // Password
+                // Not Empty
+                lblPasswordError.Text = PASSWORD_MISMATCH;
+                lblPasswordError.Visible = true;
+                errorsExist = true;
+            }
+            // Minimum Length
+            else if (tbPassword.Text.Length < 8)
+            {
+                lblPasswordError.Text = PASSWORD_TOO_SHORT;
+                lblPasswordError.Visible = true;
+                errorsExist = true;
+
+            }
+            // Check Password longer then 8 charecters and no spaces
+            else if (tbPassword.Text.Contains(' '))
+            {
+                lblPasswordError.Text = PASSWORD_INVALID_CHARECTERS;
+                lblPasswordError.Visible = true;
+                errorsExist = true;
+
+            }
+            // Matches Second
+            else if (tbPassword.Text == tbPasswordConfirm.Text)
+            {
+                lblConfirmPassword.Text = PASSWORD_MISMATCH;
+                lblConfirmPassword.Visible = true;
+                errorsExist = true;
+
+            }
+            // Security Question
+            else if (tbSecuirtyQuestionAnswer.Text == "")
+            {
+                lblSecuirtyQuestionError.Text = SECURITY_QUESTION_EMPTY;
+                lblSecuirtyQuestionError.Visible = true;
+                errorsExist = true;
+
+            }
+
+            if (!errorsExist)
+                btnCreateAccount.Enabled = false;
+            else
+                btnCreateAccount.Enabled = true;
+    
+
+        }
+
+        private void CreateProfile_Load(object sender, EventArgs e)
+        {
+            cbSecurityQuestion.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// Finds if username exists in database
+        /// </summary>
+        /// <param name="name">Name to check for, make lowercase for seach compare. stored as userentered.</param>
+        /// <returns>boolen</returns>
+        private bool FindUser(string name)
+        {
+            var user = doc.Descendants("Users").Where(s => (string)s.Attribute("Username") == name.ToLower());
+
+
+            return false;
+        }
+
+        private void btnDebugPopulate_Click(object sender, EventArgs e)
+        {
+            DebugUser();
+        }
     }
 }
