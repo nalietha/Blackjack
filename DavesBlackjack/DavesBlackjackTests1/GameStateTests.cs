d042c80596536048fa2c472d0edbe7fc2b994e88
Martin Erck <unknownmle107@gmail.com>
2019-11-21 12:17:16 -0600

    Added Unit Tests

---
+++ DavesBlackjack/DavesBlackjackTests1/GameStateTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DavesBlackjack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavesBlackjack.Tests
+{
+    [TestClass()]
+    public class GameStateTests
+    {
+        [TestMethod()]
+        public void Write_XML_File_From_GameState_Test()
+        {
+            //Arrange
+            GameState gamestate = new GameState(new Dealer(), new Player(), new Deck(), 25, true);
+            string SaveFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Blackjack Saves//";
+
+            //Act
+            gamestate.SaveGame("Test", SaveFilePath);
+
+            //Assert
+            Assert.AreEqual(true, System.IO.File.Exists(SaveFilePath + "Test.xml"));
+            if(System.IO.File.Exists(SaveFilePath + "Test.xml"))
+            {
+                System.IO.File.Delete(SaveFilePath + "Test.xml");
+            }
+        }
+    }
+}
